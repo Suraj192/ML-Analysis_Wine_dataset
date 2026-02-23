db46eda806daaacda6f662c8f601d8e719afe7b5
Suraj <59330950+Suraj192@users.noreply.github.com>
2026-02-23 20:09:32 +0200

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,1 +1,15 @@
+from sklearn.ensemble import RandomForestClassifier
 
+
+def train_model(X_train, y_train):
+    """
+    Train Random Forest model.
+    """
+    model = RandomForestClassifier(
+        n_estimators=100,
+        random_state=42
+    )
+
+    model.fit(X_train, y_train)
+
+    return model
